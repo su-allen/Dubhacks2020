8541b7282b75fc164af9ccf91abecb1d10b7db79
Jeremy Gunawan <sirknightj@gmail.com>
2020-10-17 20:26:48 -0700

    Added classroom display

--- my-app/Frontend/src/Classroom.js
+++ my-app/Frontend/src/Classroom.js
@@ -1,15 +1,35 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
 
 class Classroom extends Component {
     constructor(props) {
         super(props);
+        console.log(this.props.students);
+    }
+
+    getBadges = (student) => {
+        let out = "";
+        if (student.badges) {
+            student.badges.forEach(badge => {
+                out += badge + " ";
+            });
+            out.trim();
+        }
+        return out;
     }
 
     render() {
         return (
             <div className="Classroom">
-                <p>Classroom</p>
+                {this.props.students.sort((o1, o2) => {
+                    return o2.ranking || 0 - o1.ranking || 0;
+                }).map((student) =>
+                    <div className="StudentListView" key={student.name}>
+                        <span className="ranking">{`${student.ranking}. `}</span>
+                        <span className="username">{student.name}</span>
+                        <span className="score">{" " + student.total_points}</span>
+                        <span className="badges">{" " + this.getBadges(student)}</span>
+                    </div>
+                )}
             </div>
         );
     }
