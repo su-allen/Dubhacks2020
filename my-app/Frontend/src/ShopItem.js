b6d10d1ebe84dc08453f6b2259f825246c00d696
Jeremy Gunawan <sirknightj@gmail.com>
2020-10-18 03:36:24 -0700

    Made shop items circular

--- my-app/Frontend/src/ShopItem.js
+++ my-app/Frontend/src/ShopItem.js
@@ -11,19 +11,26 @@ class ShopItem extends Component {
         this.props.handleShopBuy(type, color);
     }
 
-    alreadyOwned = (color) => {
+    alreadyOwned = () => {
         if (this.props.type === "frame_name") {
-            return this.props.me.frame_collection && this.props.me.frame_collection.includes(color);
+            return this.props.me.frame_collection && this.props.me.frame_collection.includes(this.props.color);
         } else {
-            return this.props.me.name_collection && this.props.me.name_collection.includes(color);
+            return this.props.me.name_collection && this.props.me.name_collection.includes(this.props.color);
         }
+    }
+
+    getInside = () => {
+        if (this.props.type === "frame_color") {
+            return(<img src="profile_picture.png" className="ShopItem_Round"></img>);
+        }
+        return (<p className="ShopItem_Rect" contenteditable={false} style={(this.props.type === "frame_color") ? ((this.props.color === "Black") ? {color: "#b0aeae"} : {color: "black"}) : {color: this.props.color}}>{`${this.props.color} ${this.props.type.replace(/_/g, " ")}`}</p>);
     }
 
     render() {
         return (
             <div className="ShopItemHolder">
-                <button disabled={this.props.disabled} title={this.props.disabled ? (this.alreadyOwned(this.props.color) ? "You already own this item" : "You don't have enough to buy this item. Cost: 100 points") : "Cost: 100 points"} onClick={() => this.purchase(this.props.type, this.props.color)} className="ShopItem" style={(this.props.type === "frame_color") ? {backgroundColor: this.props.color} : {backgroundColor: "#b0aeae"}}>
-                    <p className="ShopItem" contenteditable={false} style={(this.props.type === "frame_color") ? ((this.props.color === "Black") ? {color: "#b0aeae"} : {color: "black"}) : {color: this.props.color}}>{`${this.props.color} ${this.props.type.replace(/_/g, " ")}`}</p>
+                <button disabled={this.props.disabled} title={this.props.disabled ? (this.alreadyOwned() ? "You already own this item" : "You don't have enough to buy this item. Cost: 100 points") : "Cost: 100 points"} onClick={() => this.purchase(this.props.type, this.props.color)} className={this.props.type === "frame_color" ? "ShopItem_Round" : "ShopItem_Rect"} style={(this.props.type === "frame_color") ? {backgroundColor: this.props.color} : {backgroundColor: "#b0aeae"}}>
+                    {this.getInside()}
                 </button>
             </div>
         );
